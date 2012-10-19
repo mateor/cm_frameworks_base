0e0434ff697479315a38381a8cc4d66acb4e467f
Jesús David <jedga_95@outlook.com>
2012-10-19 15:54:34 -0500

    Remove whitespaces
    Change-Id: I58109373be0dd8cb440294d77c4251b5499b699b

--- core/java/android/app/ColorPickerDialog.java
+++ core/java/android/app/ColorPickerDialog.java
@@ -87,15 +87,15 @@ public class ColorPickerDialog extends Dialog implements
         mColorPicker.setColor(color, true);
         mHex.setText(ColorPickerPreference.convertToARGB(color));
         mHex.addTextChangedListener(new TextWatcher() {
-         
+
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
-         
+
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
-         
+
             @Override
             public void afterTextChanged(Editable editable) {
                 String text = mHex.getText().toString();
