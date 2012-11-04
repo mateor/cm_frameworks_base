mateor/cm_frameworks_base

commit 91472c0390aed5ac1aef2c733f71e0b2919f6eb1

Standarization
Indenting, variable names, etc

Patchset 2: whitespaces

Change-Id: Ibe896034409c94cd37835bddfd9c3a7ccca70761

core/java/android/app/ActivityThread.java
@@ -2699,31 +2699,31 @@ public final class ActivityThread {
                     try {
                         for (int i = 0; i < ExtendedPropertiesUtils.PARANOID_COLORS_COUNT; i++) {
                             // Fetch defaults
-                            String mSetting = Settings.System.getString(r.activity.getContentResolver(),
-                                ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i]);
+                            String setting = Settings.System.getString(r.activity.getContentResolver(),
+                                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i]);
 
-                            String[] mColors = (mSetting == null || mSetting.equals("") ?
-                                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i] : mSetting).split(
-                                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                            String[] colors = (setting == null || setting.equals("") ?
+                                   ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i] : setting).split(
+                                   ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
 
                             // Sanity check
-                            if (mColors.length != 3) {
-                                mColors = ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i].split(
-                                    ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                            if (colors.length != 3) {
+                                colors = ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i].split(
+                                       ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
                                 Settings.System.putString(r.activity.getContentResolver(),
-                                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i], 
-                                    ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i]);
+                                       ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i],
+                                       ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[i]);
                             }
 
                             // Change color
-                            String mCurColor = mColors[Integer.parseInt(mColors[2])];
+                            String mCurColor = colors[Integer.parseInt(colors[2])];
                             String mAppColor = ExtendedPropertiesUtils.mGlobalHook.colors[i];
-                            String mNexColor = mAppColor.equals("") ? mColors[0] : mAppColor;
+                            String mNexColor = mAppColor.equals("") ? colors[0] : mAppColor;
 
                             if (mNexColor != mCurColor) {
                                 Settings.System.putString(r.activity.getContentResolver(),
-                                    ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i], 
-                                    mColors[0] + "|" + mNexColor + "|1");
+                                       ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i],
+                                       colors[0] + "|" + mNexColor + "|1");
                             }
                         }
                     } catch (Exception e) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -581,11 +581,11 @@ public class NavigationBarView extends LinearLayout {
         }
 
         String mSetting = Settings.System.getString(mContext.getContentResolver(),
-            Settings.System.NAV_BAR_COLOR);
+                Settings.System.NAV_BAR_COLOR);
         String[] mColors = (mSetting == null || mSetting.equals("") ?
-            ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-            ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
-            ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         String mCurColor = mColors[Integer.parseInt(mColors[2])];
         
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
@@ -593,7 +593,7 @@ public class NavigationBarView extends LinearLayout {
         cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
-            getBackground(), new BitmapDrawable(bm)});
+                getBackground(), new BitmapDrawable(bm)});
         transition.setCrossFadeEnabled(true);
         setBackground(transition);
         transition.startTransition(1000);

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -39,20 +39,20 @@ import android.content.res.CustomTheme;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
-import android.graphics.drawable.NinePatchDrawable;
 import android.inputmethodservice.InputMethodService;
 import android.os.Handler;
 import android.os.IBinder;
@@ -120,8 +120,8 @@ import com.android.systemui.statusbar.policy.NotificationRowLayout;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class PhoneStatusBar extends BaseStatusBar {
     static final String TAG = "PhoneStatusBar";
@@ -340,12 +340,12 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
 
         String mSetting = Settings.System.getString(mContext.getContentResolver(),
-            Settings.System.STATUS_BAR_COLOR);
+                Settings.System.STATUS_BAR_COLOR);
 
         String[] mColors = (mSetting == null || mSetting.equals("") ?
-            ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-            ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR] : mSetting).split(
-            ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
+                ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         String mCurColor = mColors[Integer.parseInt(mColors[2])];
 
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
@@ -353,7 +353,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
-            mStatusBarView.getBackground(), new BitmapDrawable(bm)});
+                mStatusBarView.getBackground(), new BitmapDrawable(bm)});
         transition.setCrossFadeEnabled(true);
         mStatusBarView.setBackground(transition);
         transition.startTransition(1000);

packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -34,7 +34,6 @@ import android.os.SystemClock;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.ExtendedPropertiesUtils;
-import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
@@ -140,16 +139,16 @@ public class KeyButtonView extends ImageView {
         }
 
         String mSetting = Settings.System.getString(mContext.getContentResolver(),
-            Settings.System.NAV_BUTTON_COLOR);
+                Settings.System.NAV_BUTTON_COLOR);
 
         String[] mButtonColors = (mSetting == null || mSetting.equals("") ?
-            ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-            ExtendedPropertiesUtils.PARANOID_COLORS_NAVBUTTON] : mSetting).split(
-            ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBUTTON] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         String mCurButtonColor = mButtonColors[Integer.parseInt(mButtonColors[2])];
 
         setColorFilter(new BigInteger("FF" + mCurButtonColor.substring(2), 16).intValue(),
-            PorterDuff.Mode.SRC_ATOP);
+                PorterDuff.Mode.SRC_ATOP);
 
         BUTTON_QUIESCENT_ALPHA = (float)new BigInteger(mCurButtonColor.substring(0, 2), 16).intValue() / 255f;
         setDrawingAlpha(BUTTON_QUIESCENT_ALPHA);
@@ -157,16 +156,16 @@ public class KeyButtonView extends ImageView {
 
     private void updateGlowColor() {
         String mSetting = Settings.System.getString(mContext.getContentResolver(),
-            Settings.System.NAV_GLOW_COLOR);
+                Settings.System.NAV_GLOW_COLOR);
 
         String[] mGlowColors = (mSetting == null || mSetting.equals("") ?
-            ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-            ExtendedPropertiesUtils.PARANOID_COLORS_NAVGLOW] : mSetting).split(
-            ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVGLOW] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         String mCurGlowColor = mGlowColors[Integer.parseInt(mGlowColors[2])];
 
         mGlowBG.setColorFilter(new BigInteger(mCurGlowColor, 16).intValue(),
-            PorterDuff.Mode.SRC_ATOP);
+                PorterDuff.Mode.SRC_ATOP);
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBarView.java
@@ -179,11 +179,11 @@ public class TabletStatusBarView extends FrameLayout {
         }
 
         String mSetting = Settings.System.getString(mContext.getContentResolver(),
-            Settings.System.NAV_BAR_COLOR);
+                Settings.System.NAV_BAR_COLOR);
         String[] mColors = (mSetting == null || mSetting.equals("") ?
-            ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-            ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
-            ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+                ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         String mCurColor = mColors[Integer.parseInt(mColors[2])];
         
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
@@ -191,7 +191,7 @@ public class TabletStatusBarView extends FrameLayout {
         cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
-            getBackground(), new BitmapDrawable(bm)});
+                getBackground(), new BitmapDrawable(bm)});
         transition.setCrossFadeEnabled(true);
         setBackground(transition);
         transition.startTransition(1000);

core/java/android/util/ExtendedPropertiesUtils.java
@@ -113,7 +113,7 @@ public class ExtendedPropertiesUtils {
         public int large;
         public float scaledDensity;
         public float density;
-        public String [] colors = {"", "", "", ""};
+        public String[] colors = new String[4];
     }
 
     /**
@@ -163,11 +163,11 @@ public class ExtendedPropertiesUtils {
             info.large = Integer.parseInt(getProperty(info.name + PARANOID_LARGE_SUFFIX));
 
             // Color parameters
-            String mColors = getProperty(info.name + PARANOID_COLORS_SUFFIX);
-            String[] mColorArray = mColors.split(PARANOID_STRING_DELIMITER);
-            if (mColorArray.length == PARANOID_COLORS_COUNT) {
-                for(int i=0; i < mColorArray.length; i++) {
-                    info.colors[i] = mColorArray[i].toUpperCase();
+            String[] colors = getProperty(info.name +
+                    PARANOID_COLORS_SUFFIX).split(PARANOID_STRING_DELIMITER);
+            if (colors.length == PARANOID_COLORS_COUNT) {
+                for(int i=0; i < colors.length; i++) {
+                    info.colors[i] = colors[i].toUpperCase();
                 }
             }
 

commit a3fa328d79a3336cc7e537ccae070139e0520fcc

Per app color fix
Fix default color fallback broken by commit 91472c0390aed5ac1aef2c733f71e0b2919f6eb1

Change-Id: Id526b417366f7be73547714002e2e5dc96c60006

core/java/android/app/ActivityThread.java
@@ -2716,14 +2716,14 @@ public final class ActivityThread {
                             }
 
                             // Change color
-                            String mCurColor = colors[Integer.parseInt(colors[2])];
-                            String mAppColor = ExtendedPropertiesUtils.mGlobalHook.colors[i];
-                            String mNexColor = mAppColor.equals("") ? colors[0] : mAppColor;
+                            String currentColor = colors[Integer.parseInt(colors[2])];
+                            String appColor = ExtendedPropertiesUtils.mGlobalHook.colors[i];
+                            String nextColor = appColor == null ? colors[0] : appColor;
 
-                            if (mNexColor != mCurColor) {
+                            if (nextColor != currentColor) {
                                 Settings.System.putString(r.activity.getContentResolver(),
                                        ExtendedPropertiesUtils.PARANOID_COLORS_SETTINGS[i],
-                                       colors[0] + "|" + mNexColor + "|1");
+                                       colors[0] + "|" + nextColor + "|1");
                             }
                         }
                     } catch (Exception e) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -578,17 +578,17 @@ public class NavigationBarView extends LinearLayout {
             return;
         }
 
-        String mSetting = Settings.System.getString(mContext.getContentResolver(),
+        String setting = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.NAV_BAR_COLOR);
-        String[] mColors = (mSetting == null || mSetting.equals("") ?
+        String[] colors = (setting == null || setting.equals("")  ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : setting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
-        String mCurColor = mColors[Integer.parseInt(mColors[2])];
+        String currentColor = colors[Integer.parseInt(colors[2])];
         
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
         Canvas cnv = new Canvas(bm);
-        cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
+        cnv.drawColor(new BigInteger(currentColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
                 getBackground(), new BitmapDrawable(bm)});

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -339,18 +339,17 @@ public class PhoneStatusBar extends BaseStatusBar {
             return;
         }
 
-        String mSetting = Settings.System.getString(mContext.getContentResolver(),
+        String setting = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.STATUS_BAR_COLOR);
-
-        String[] mColors = (mSetting == null || mSetting.equals("") ?
+        String[] colors = (setting == null || setting.equals("")  ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : setting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
-        String mCurColor = mColors[Integer.parseInt(mColors[2])];
+        String currentColor = colors[Integer.parseInt(colors[2])];
 
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
         Canvas cnv = new Canvas(bm);
-        cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
+        cnv.drawColor(new BigInteger(currentColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
                 mStatusBarView.getBackground(), new BitmapDrawable(bm)});

packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -138,33 +138,33 @@ public class KeyButtonView extends ImageView {
             return;
         }
 
-        String mSetting = Settings.System.getString(mContext.getContentResolver(),
+        String setting = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.NAV_BUTTON_COLOR);
 
-        String[] mButtonColors = (mSetting == null || mSetting.equals("") ?
+        String[] buttonColors = (setting == null || setting.equals("") ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBUTTON] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBUTTON] : setting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
-        String mCurButtonColor = mButtonColors[Integer.parseInt(mButtonColors[2])];
+        String currentColor = buttonColors[Integer.parseInt(buttonColors[2])];
 
-        setColorFilter(new BigInteger("FF" + mCurButtonColor.substring(2), 16).intValue(),
+        setColorFilter(new BigInteger("FF" + currentColor.substring(2), 16).intValue(),
                 PorterDuff.Mode.SRC_ATOP);
 
-        BUTTON_QUIESCENT_ALPHA = (float)new BigInteger(mCurButtonColor.substring(0, 2), 16).intValue() / 255f;
+        BUTTON_QUIESCENT_ALPHA = (float)new BigInteger(currentColor.substring(0, 2), 16).intValue() / 255f;
         setDrawingAlpha(BUTTON_QUIESCENT_ALPHA);
     }
 
     private void updateGlowColor() {
-        String mSetting = Settings.System.getString(mContext.getContentResolver(),
+        String setting = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.NAV_GLOW_COLOR);
 
-        String[] mGlowColors = (mSetting == null || mSetting.equals("") ?
+        String[] glowColors = (setting == null || setting.equals("") ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_NAVGLOW] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVGLOW] : setting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
-        String mCurGlowColor = mGlowColors[Integer.parseInt(mGlowColors[2])];
+        String currentColor = glowColors[Integer.parseInt(glowColors[2])];
 
-        mGlowBG.setColorFilter(new BigInteger(mCurGlowColor, 16).intValue(),
+        mGlowBG.setColorFilter(new BigInteger(currentColor, 16).intValue(),
                 PorterDuff.Mode.SRC_ATOP);
     }
 

packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBarView.java
@@ -178,17 +178,17 @@ public class TabletStatusBarView extends FrameLayout {
             return;
         }
 
-        String mSetting = Settings.System.getString(mContext.getContentResolver(),
+        String setting = Settings.System.getString(mContext.getContentResolver(),
                 Settings.System.NAV_BAR_COLOR);
-        String[] mColors = (mSetting == null || mSetting.equals("") ?
+        String[] colors = (setting == null || setting.equals("")  ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : setting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
-        String mCurColor = mColors[Integer.parseInt(mColors[2])];
+        String currentColor = colors[Integer.parseInt(colors[2])];
         
         Bitmap bm = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
         Canvas cnv = new Canvas(bm);
-        cnv.drawColor(new BigInteger(mCurColor, 16).intValue());
+        cnv.drawColor(new BigInteger(currentColor, 16).intValue());
 
         TransitionDrawable transition = new TransitionDrawable(new Drawable[]{
                 getBackground(), new BitmapDrawable(bm)});