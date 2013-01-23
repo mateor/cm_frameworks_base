bd9e4903ad77b9314f4925cf684e0473e224f77f
mateor <mateorod9@gmail.com>
2013-01-23 13:34:32 -0600

    TabletUI initial commit
    Also see packages/apps/Settings: 5e88c69f73ac69d8189265a595ab7fdb20725b3a
    
    Change-Id: Ie2c55978b128b5ccd223c7f699e742c98f760efd
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavbarEditor.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -6803,7 +6803,7 @@ public class WindowManagerService extends IWindowManager.Stub
         sl = reduceConfigLayout(sl, Surface.ROTATION_90, density, unrotDh, unrotDw);
         sl = reduceConfigLayout(sl, Surface.ROTATION_180, density, unrotDw, unrotDh);
         sl = reduceConfigLayout(sl, Surface.ROTATION_270, density, unrotDh, unrotDw);
-        outConfig.smallestScreenWidthDp = (int)(displayInfo.smallestNominalAppWidth / density);
+        outConfig.smallestScreenWidthDp = 721;
         outConfig.screenLayout = sl;
     }
 
