66bb67a3c052dd7c2ba368e5b140acba7cf0c2f7
mateor <mateorod9@gmail.com>
2013-02-21 01:34:48 -0600

    AppWindowToken.java resolved
    Change-Id: I903f87f727b161b8ff2065c808528cec13e8c5a1

--- services/java/com/android/server/wm/AppWindowToken.java
+++ services/java/com/android/server/wm/AppWindowToken.java
@@ -101,10 +101,21 @@ class AppWindowToken extends WindowToken {
     // Input application handle used by the input dispatcher.
     final InputApplicationHandle mInputApplicationHandle;
 
+    /**
+     * Author: Onskreen
+     * Date: 11/02/2010
+     *
+     * Flag indiciating if token is the cornerstone.
+     */
+    boolean isCornerstone;
+
+    final WindowManagerService mService;
+
     AppWindowToken(WindowManagerService _service, int _userId, IApplicationToken _token) {
         super(_service, _token.asBinder(),
                 WindowManager.LayoutParams.TYPE_APPLICATION, true);
         userId = _userId;
+        mService = _service;
         appWindowToken = this;
         appToken = _token;
         mInputApplicationHandle = new InputApplicationHandle(this);
